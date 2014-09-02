4570104d47403a346d1f84704722a76c2ad13eca
Rossano <rossanorb@gmail.com>
2014-09-02 16:10:44 -0300

    Removido pasta Controllers, e configurado o bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -4,13 +4,11 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
     protected function _initDoctype()
     {        
-        $this->bootstrap('view');
-        $view = $this->getResource('view');
-        $view->doctype('HTML5');
-        
+        //resources.frontController.controllerDirectory = APPLICATION_PATH "/controllers"
        // Zend_Controller_Front::getInstance()        
        //     ->setParam('useDefaultControllerAlways', true);        // irá redirecionar sempre para o controlador padrão
     }   
-
+    
+    
 }
 
