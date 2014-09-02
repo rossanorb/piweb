484bca2c62c4436ef7c8e124a162ae576fd49dea
Rossano <rossanorb@gmail.com>
2014-09-02 20:56:31 -0300

    Alteração do IndexController do módulo clínica

--- application/modules/clinica/controllers/IndexController.php
+++ application/modules/clinica/controllers/IndexController.php
@@ -5,10 +5,7 @@ class Clinica_IndexController extends Zend_Controller_Action{
     public function indexAction(){
        // $model =   new Teste_Class_Hello();
         //$this->_helper->layout->setLayout('site');
-        $this->view->mensagem = ' Module: Clinica  / Controller: index / Action: index  ';        
-         $user = new Application_Model_User();
-         var_dump($user);
-         
+        $this->view->mensagem = ' Module: Clinica  / Controller: index / Action: index  ';          
     }
     
     public function showAction(){
