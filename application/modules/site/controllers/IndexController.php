6204b277c6e50329c1b51db5920f57f62ddfd93d
Rossano <rossanorb@gmail.com>
2014-09-02 21:39:45 -0300

    inclusão do $this->layout->content

--- application/modules/site/controllers/IndexController.php
+++ application/modules/site/controllers/IndexController.php
@@ -3,17 +3,10 @@
 class Site_IndexController extends Zend_Controller_Action{
     
     public function indexAction(){
-       // $model =   new Teste_Class_Hello();
-        //$this->_helper->layout->setLayout('site');
-        $this->view->mensagem = ' Module: Site  / Controller: index / Action: index  ';
-         //$user = new Application_Model_User();
-         //var_dump($user);
-         
         
-         
     }
     
-    public function showAction(){
-        $this->view->mensagem =  ' Module: Site  / Controller: index / Action: show  ';
+    public function buscaAction(){
+        
     }
 }
