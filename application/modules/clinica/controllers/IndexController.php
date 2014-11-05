3532f7e7b8a9f1efc46704cb656adc1402f6f0d1
Rossano <rossanorb@gmail.com>
2014-11-04 22:15:14 -0200

    layout login clínica

--- application/modules/clinica/controllers/IndexController.php
+++ application/modules/clinica/controllers/IndexController.php
@@ -2,23 +2,34 @@
 
 class Clinica_IndexController extends Zend_Controller_Action{
     
-    public function indexAction(){
-        $db = Zend_Db_Table::getDefaultAdapter();
-        $sql = 'Select * from posts';        
-        $query = $db->query($sql);
-        $rows = $query->fetchAll();        
-        
-        foreach ($rows as $row){
-            print '<p>Título: '.$row['title'] .'<p>';
-        }
+//    public function indexAction(){
+//        $db = Zend_Db_Table::getDefaultAdapter();
+//        $sql = 'Select * from posts';        
+//        $query = $db->query($sql);
+//        $rows = $query->fetchAll();        
+//        
+//        foreach ($rows as $row){
+//            print '<p>Título: '.$row['title'] .'<p>';
+//        }
+//    }
+//    
+//    public function showAction(){
+//        $this->view->mensagem =  ' Module: Clínica  / Controller: index / Action: show  ';
+//    }
+//    
+//    public function formAction(){
+//        $form = new Form_User();
+//        var_dump($form);
+//    }
+    public function init() {
+        parent::init();
+         $this->_helper->layout->setLayout('authclinica');
     }
     
-    public function showAction(){
-        $this->view->mensagem =  ' Module: Clínica  / Controller: index / Action: show  ';
+    public function loginAction(){
+        $this->view->form = new Form_LoginClinica();
     }
+            
     
-    public function formAction(){
-        $form = new Form_User();
-        var_dump($form);
-    }
+    
 }
