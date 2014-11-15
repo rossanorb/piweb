a198990c58807405b7506b16c7425a5fd084efe0
Rossano <rossanorb@gmail.com>
2014-11-15 16:16:47 -0200

    Alteração do menu pacientes

--- application/modules/clinica/controllers/IndexController.php
+++ application/modules/clinica/controllers/IndexController.php
@@ -2,23 +2,42 @@
 
 class Clinica_IndexController extends Zend_Controller_Action{
     
+//    public function indexAction(){
+//        $db = Zend_Db_Table::getDefaultAdapter();
+//        $sql = 'Select * from posts';        
+//        $query = $db->query($sql);
+//        $rows = $query->fetchAll();        
+//        
+//        foreach ($rows as $row){
+//            print '<p>Título: '.$row['title'] .'<p>';
+//        }
+//    }
+//    
+//    public function showAction(){
+//        $this->view->mensagem =  ' Module: Clínica  / Controller: index / Action: show  ';
+//    }
+//    
+//    public function formAction(){
+//        $form = new Form_User();
+//        var_dump($form);
+//    }
+    
+    public function init() {        
+        parent::init();        
+         $auth = Zend_Auth::getInstance();
+         if(!$auth->hasIdentity()) $this->_redirect ('site/clinica/login');
+         
+    }
+    
     public function indexAction(){
-        $db = Zend_Db_Table::getDefaultAdapter();
-        $sql = 'Select * from posts';        
-        $query = $db->query($sql);
-        $rows = $query->fetchAll();        
-        
-        foreach ($rows as $row){
-            print '<p>Título: '.$row['title'] .'<p>';
-        }
+        $this->view->mensagem = 'logado';        
     }
     
-    public function showAction(){
-        $this->view->mensagem =  ' Module: Clínica  / Controller: index / Action: show  ';
+    public function logoutAction() {
+            Zend_Auth::getInstance()->clearIdentity();
+            $this->_redirect('clinica/index/');
     }
     
-    public function formAction(){
-        $form = new Form_User();
-        var_dump($form);
-    }
+    
+    
 }
